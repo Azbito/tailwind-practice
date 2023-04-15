7194ab6e48e4a7e4ab168154171c94d2a8060388
Thiago <101950809+Azbito@users.noreply.github.com>
2023-04-15 01:08:45 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    container: {
+      backgroundColor: 'black'
+    },
+    extend: {}
+  },
+  plugins: []
+}
